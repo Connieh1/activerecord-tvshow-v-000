b722c336c0629c290bd35ffbbad3bb47ca38ee47
Connie Hyman <constance.hyman@mchsi.com>
2018-09-20 19:32:50 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -13,7 +13,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.least_popular_show
-    Show.find_by(lowest_rating).sort
+    Show.find_by(lowest_rating).sort.reverse
   end
 
   def self.ratings_sum
